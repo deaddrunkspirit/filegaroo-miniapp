2c685ea01e4a635f438f8ab15be388b2d8a549e0
deaddrunkspirit <dedrsp@gmail.com>
2024-01-09 22:44:36 +0100

    fixed icon mapping

--- webapp/src/services/utils.ts
+++ webapp/src/services/utils.ts
@@ -1,21 +1,26 @@
 import { ContentType } from "../types/content";
 
 export const getIconPath = (content: ContentType) => {
+    const contentType = content.attachment ?? 1
     const iconMapping = {
         0: 'folder-icon.svg',
         1: 'text-icon.svg',
         2: 'audio-icon.svg',
         3: 'file-icon.svg',
         4: 'image-icon.svg',
-        5: 'image-icon.svg',
-        6: 'video-icon.svg',
-        7: 'voice-message-icon.svg',
-        8: 'video-icon.svg',
-        9: 'image-icon.svg',
-        10: 'image-icon.svg',
-        11: 'image-icon.svg',
+        5: 'video-icon.svg',
+        6: 'voice-message-icon.svg'
       };
-      const index: keyof typeof iconMapping = content.attachment && content.attachment !== null ? (content.attachment as keyof typeof iconMapping) : 0;
+      
+      let index: keyof typeof iconMapping;
+
+      if (content.attachment && content.attachment !== null) {
+        index = contentType as keyof typeof iconMapping;
+      } else if (content.type === 1) {
+        index = contentType as keyof typeof iconMapping;
+      } else {
+        index = 0;
+      }
       const iconFileName: string = iconMapping[index] || 'default_icon.svg';
     return process.env.PUBLIC_URL + `/icons/${iconFileName}`;
   };
