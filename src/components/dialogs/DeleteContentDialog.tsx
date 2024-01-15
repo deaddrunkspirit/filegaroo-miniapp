219bba5e9d355e3654fa756d001c8d087d632a42
deaddrunkspirit <dedrsp@gmail.com>
2024-01-15 09:01:44 +0100

    small fix

--- src/components/dialogs/DeleteContentDialog.tsx
+++ src/components/dialogs/DeleteContentDialog.tsx
@@ -34,7 +34,7 @@ const DeleteContentDialog: React.FC<DeleteContentDialogProps> = ({onEnd, content
     return (
         <div className='flex flex-col w-52 h-32  justify-around py-3 items-stretch bg-color_input dark:bg-dark-secondary bg-opacity-80 dark:bg-opacity-80 rounded-3xl shadow-2xl'>
           <div className='flex flex-row items-center px-4 gap-4 justify-center' onClick={handleDelete}>
-            <p className="flex text-center text-md text-light-onprimary dark:text-dark-onsecondary">Вы действительно хотите удалить {content.type == 1 ? 'папку' : 'сообщение'}?</p>
+            <p className="flex text-center text-md text-light-onprimary dark:text-dark-onsecondary">Вы действительно хотите удалить {content.type == 2 ? 'папку' : 'сообщение'}?</p>
           </div>
           <div className='w-full h-[2px] opacity-20 bg-light-onprimary dark:bg-dark-onsecondary' />
           <div className='flex flex-row items-center px-4 gap-4 justify-center' onClick={handleDelete}>
