95f2aed9c9187b7d4fdc6c97813b9a67c3350aaa
deaddrunkspirit <dedrsp@gmail.com>
2024-01-18 12:50:08 +0100

    ui fix

--- src/components/cards/ContentCard.tsx
+++ src/components/cards/ContentCard.tsx
@@ -46,7 +46,7 @@ const ContentCard: React.FC<ContentCardProps> = ({ content }) => {
                 className='relative z-20 flex flex-col gap-[2vw] items-center justify-center w-[39vw] h-[31vw] px-[3.8vw] pb-[2.56vw] pt-[5.475vw] rounded-3xl dark:drop-shadow-xl bg-light-secondary text-light-onsecondary  dark:bg-dark-secondary dark:text-dark-onsecondary m-0 shadow-lg shadow-gray-400 dark:shadow-black' 
             >
             <div className="flex items-center w-[22vw] h-[13.5vw] justify-center">
-                <img className='w-full h-auto' src={getContentImage(content)} alt="Content Icon" />
+                <img className=' object-contain' src={getContentImage(content)} alt="Content Icon" />
             </div>
             <p className='flex grow break-all h-[7.2vw] text-sm leading-[3.70vw] line-clamp-2 text-ellipsis overflow-hidden ...'>
                 {content.title}
