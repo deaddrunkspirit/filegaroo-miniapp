b63a2f2adcf980571e86827557edef75a9f33451
deaddrunkspirit <dedrsp@gmail.com>
2024-01-20 23:55:16 +0100

    added FAQ link

--- src/components/headers/MainPageHeader.tsx
+++ src/components/headers/MainPageHeader.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import getLocalizationString from '../../services/languageService';
 import { useTelegramContext } from '../../providers/TelegramContext';
-// import { getIcon } from '../../services/imageService';
-// import { NavLink } from 'react-router-dom';
+import { getIcon } from '../../services/imageService';
+import { NavLink } from 'react-router-dom';
 
 const MainPageHeader: React.FC = () => {
-  const { lang } = useTelegramContext();
+  const { lang, colorScheme } = useTelegramContext();
 
   return (
     <div className="flex flex-row self-start w-full h-[18.5vw] pl-[8vw] pt-[6.39vw] pr-[7.22vw] pb-[5.56vw] gap-[4.72vw] bg-light-primary text-light-onprimary dark:bg-dark-primary dark:text-dark-onprimary">
@@ -13,10 +13,10 @@ const MainPageHeader: React.FC = () => {
       <div className='grow' />
       {/* <NavLink to="/settings" end>
         <img className="w-6 h-6 m-0" src={getIcon('settings', colorScheme!)} alt="Settings" />
-      </NavLink>
-      <NavLink to="/faq" end>
-        <img className="w-6 h-6 m-0" src={getIcon('faq', colorScheme!)} alt="FAQ" />
       </NavLink> */}
+      <NavLink to="/faq" end>
+        <img className="h-[6.11vw] w-[6.11vw] m-0" src={getIcon('faq', colorScheme!)} alt="FAQ" />
+      </NavLink>
     </div>
   );
 }
