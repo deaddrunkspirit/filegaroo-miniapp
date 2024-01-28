0e89b3678ca39dc49672f3d2bb2e4342193132fc
deaddrunkspirit <dedrsp@gmail.com>
2024-01-28 02:03:41 +0100

    actually now skeletons ready

--- src/components/headers/MoveChooseFolderHeader.tsx
+++ src/components/headers/MoveChooseFolderHeader.tsx
@@ -10,23 +10,23 @@ type MoveChooseFolderHeaderProps = {
     onFolderChanged: (newId: number | null) => void;
 }
 
-const MoveChooseFolderHeader: React.FC<MoveChooseFolderHeaderProps> = ({onClose, parent, onFolderChanged}) => {
-  const { colorScheme } = useTelegramContext();
+const MoveChooseFolderHeader: React.FC<MoveChooseFolderHeaderProps> = ({ onClose, parent, onFolderChanged }) => {
+    const { colorScheme } = useTelegramContext();
 
-  return (
-    <div className="flex flex-row self-start w-full h-[18.5vw] pl-[8vw] pt-[6.39vw] pr-[7.22vw] pb-[5.56vw] gap-[4.72vw] bg-light-primary text-light-onprimary dark:bg-dark-primary dark:text-dark-onprimary">
-      { parent ?
-      <button className='h-[6.11vw] w-[6.11vw]' onClick={() => {onFolderChanged((parent ? parent.parent_content_id : null) ?? null)}}>
-        <img className="h-full w-full" src={getIcon('back', colorScheme!)} alt="Back" />
-      </button> : null
-      }
-      <h1 className='text-lg text-light-onprimary dark:text-dark-onprimary'>{parent ? parent.title : getLocalizationString('main-page-name') as string}</h1>
-      <div className='grow' />
-      <div onClick={onClose}>
-        <img className="h-[6.11vw] w-[6.11vw] m-0" src={getIcon('close', colorScheme!)} alt="close" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-row self-start w-full h-[18.5vw] pl-[8vw] pt-[6.39vw] pr-[7.22vw] pb-[5.56vw] gap-[4.72vw] bg-light-primary text-light-onprimary dark:bg-dark-primary dark:text-dark-onprimary">
+            {parent ?
+                <button className='h-[6.11vw] w-[6.11vw]' onClick={() => { onFolderChanged((parent ? parent.parent_content_id : null) ?? null) }}>
+                    <img className="h-full w-full" src={getIcon('back', colorScheme!)} alt="Back" />
+                </button> : null
+            }
+            <h1 className='text-lg text-light-onprimary dark:text-dark-onprimary'>{parent ? parent.title : getLocalizationString('main-page-name') as string}</h1>
+            <div className='grow' />
+            <div onClick={onClose}>
+                <img className="h-[5vw] w-[5vw] m-0" src={getIcon('close-page', colorScheme!)} alt="close" />
+            </div>
+        </div>
+    );
 }
 
 export default MoveChooseFolderHeader;
